f77625a9e3ea95be2c8213cb4f69f94877469d07
osboxes.org <jstone04@uoguelph.ca>
2020-12-14 19:27:42 -0500

    done with youtube tutorial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import './App.css';
-import Home from './components/pages/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Services from './components/pages/Services';
-import Products from './components/pages/Products';
-import SignUp from './components/pages/SignUp';
+import Home from './components/pages/Home';
+import Portfolio from './components/pages/Portfolio';
+import Search_Company from './components/pages/Search_Company';
 
 function App() {
   return (
@@ -15,9 +14,8 @@ function App() {
         <Switch>
           {/* <Route path='/' exact /> */}
           <Route path='/' exact component={Home} />
-          <Route path='/services' component={Services} />
-          <Route path='/products' component={Products} />
-          <Route path='/sign-up' component={SignUp} />
+          <Route path='/portfolio' component={Portfolio} />
+          <Route path='/search_sompany' component={Search_Company} />
         </Switch>
       </Router>
     </>
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,31 +1,44 @@
 import React from 'react';
 import '../App.css';
-import { Button } from './Button';
+// import { Button } from './Button';
 import './HeroSection.css';
+import { Link } from 'react-router-dom';
 
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <video src='/videos/video-1.mp4' autoPlay loop muted />
-      <h1>ADVENTURE AWAITS</h1>
-      <p>What are you waiting for?</p>
-      <div className='hero-btns'>
-        <Button
-          className='btns'
-          buttonStyle='btn--outline'
-          buttonSize='btn--large'
-        >
-          GET STARTED
-        </Button>
-        <Button
-          className='btns'
-          buttonStyle='btn--primary'
-          buttonSize='btn--large'
-          onClick={console.log('hey')}
-        >
-          WATCH TRAILER <i className='far fa-play-circle' />
-        </Button>
+      {/* <img id='stock_background' src='./images/stock_background.jpg' alt='stock background'/> */}
+      <h1>Stocker</h1>
+      <p>Your Personalized Portfolio Tracker</p>
+      <div><br/><br/></div>
+
+      <div>
+        <form>
+          <label>
+            <input type='text' placeholder='Email' size='30' style={{ fontSize:25, padding:5}}/>
+          </label>
+        </form>
       </div>
+
+      <div><br/></div>
+
+      <div>
+        <form>
+          <label>
+            <input type='text' placeholder='Password' size='30' style={{ fontSize:25, padding:5}}/>
+          </label>
+        </form>
+      </div>
+
+      <div><br/></div>
+
+      <div>
+        <Link to='/portfolio'>
+          <button id='loginBtn'>Login</button>
+        </Link>
+        
+      </div>
+
     </div>
   );
 }
